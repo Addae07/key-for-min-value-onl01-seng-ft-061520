c172c72b14c16be9ab53b3becdc03c67e0fc1805
William Lawrence <will.lawrence.3rd@gmail.com>
2020-06-30 00:33:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,7 @@
 def key_for_min_value(name_hash)
 
 end
-
-def key_for_min_value(hash)
-def key_for_min_value(name_hash)	    return nil if hash.empty?
+	    return nil if hash.empty?
 
     arr = hash.collect { |k, v| v }.sort
-end 	    hash.each { |k, v| return k if v == arr[0] }
-end
+	    hash.each { |k, v| return k if v == arr[0] }
